edfc7e8a0838ab014a8f6ac162aa476895b997a8
Conradfrmdao <godlovesconrad@gmail.com>
2025-07-23 12:35:12 +0300

    python update

---
+++ strings.py
@@ -0,0 +1,14 @@
+name='wagaba'
+print(name[0])
+find='a' in 'kampala'
+print(find)
+name=name.upper()
+print(name)
+
+count=0
+word='hello world love you'
+for letter in word:
+    if letter =='l':
+        count+=1
+print("Count of 'l' in the word is :", count)
+
