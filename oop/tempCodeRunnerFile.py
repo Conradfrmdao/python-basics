dce08e3cd6ddee932ec547e43abb919048039e9e
Conrad wagaba <godlovesconrad@gmail.com>
2025-07-30 12:37:17 +0300

    Delete oop/tempCodeRunnerFile.py

--- oop/tempCodeRunnerFile.py
+++
@@ -1,8 +0,0 @@
-class Myclass:
-    def _init_(self, a=0,b=0,c=0):
-        self.a = a
-        self.b = b
-        self.c = c
-        print(f"{a}, {b}, {c} are initialized in the class Myclass")
-
-obj = Myclass(2,3,4)  # Creates an instance of Myclass with parameters 1, 2, and 3
